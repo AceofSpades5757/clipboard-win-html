00af309865b5ed7dbe3aa0a5f1b35ae5a902e219
AceofSpades5757 <10341888+AceofSpades5757@users.noreply.github.com>
2024-04-18 23:26:30 -0500

    test: update test

--- tests/test_simple.rs
+++ tests/test_simple.rs
@@ -5,6 +5,11 @@ mod tests {
     fn test_for_no_errors() {
         use clipboard_win_html::set_clipboard_html;
 
-        set_clipboard_html("<h1>Pure, valid, HTML.</h1>".to_string());
+        // A user said they were getting errors when running this more than once. The user
+        // acutally fixed the come themself, but this should cover that case moving forward.
+        set_clipboard_html("<h1>Pure, valid, HTML.</h1>".to_string()).unwrap();
+        set_clipboard_html("<h1>2nd Pure, valid, HTML.</h1>".to_string()).unwrap();
+        set_clipboard_html("<h1>3nd Pure, valid, HTML.</h1>".to_string()).unwrap();
+        set_clipboard_html("<h1>4th Pure, valid, HTML.</h1>".to_string()).unwrap();
     }
 }
